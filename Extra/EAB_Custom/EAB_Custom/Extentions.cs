2ec884b81474aa2ee2e48e7f6957d928215bcf51
pgarratt <pgarratt@theanswerco.com>
2012-08-15 15:22:47 -0700

    return grid

--- Extra/EAB_Custom/EAB_Custom/Extentions.cs
+++ Extra/EAB_Custom/EAB_Custom/Extentions.cs
@@ -27,8 +27,37 @@ namespace EAB_Custom
     public class Extentions
     {
         // Example of target method signature
+        private static void AddEditAccountProductGroup(string Accountid, string CategoryID, Double Margin)
+        {
+            //Find if accou
+            string ID = Extentions.GetField<string>("ACCOUNTPRODUCTCATEGORYID", "ACCOUNTPRODUCTCATEGORY", "PRODUCTCATEGORYID = '" + CategoryID  + "' AND ACCOUNTID ='" + Accountid +"'");
+            if (ID != string.Empty)
+            {
+               //Create the New Item
+                Sage.Entity.Interfaces.IAccountProductCategory  item = Sage.Platform.EntityFactory.Create<Sage.Entity.Interfaces.IAccountProductCategory>();
+                item.Accountid = Accountid;
+                item.ProductCategoryID = CategoryID;
+                item.Margin = Margin;
+                item.Save();
+
+
+            }
+            else
+            {
+               // uPDATE Existing
+                Sage.Entity.Interfaces.IAccountProductCategory item = Sage.Platform.EntityFactory.GetById<Sage.Entity.Interfaces.IAccountProductCategory>(ID);
+                
+                item.Margin = Margin;
+                item.Save();
+
+
+            }
+        }
         public static void AddProductGroupItems(IStockCardItems stockcarditems, String CategoryID, IAccount Account, Double Margin)
         {
+            //Add Update the Product Category Margin
+            AddEditAccountProductGroup(Account.Id.ToString(), CategoryID, Margin);
+
             String SQL = "SELECT     PRODUCT.PRODUCTID, PRODUCT.ACTUALID, PRODUCT.DESCRIPTION, TIMPRODCATEGORY.TIMPRODCATEGORYID, ";
             SQL += "                      TIMPRODCATEGORY.PRODCATEGORYID,COMPANYID";
             SQL += " FROM         PRODUCT INNER JOIN";
@@ -87,5 +116,51 @@ namespace EAB_Custom
             }
 
         }
+
+
+        #region Utility
+
+        public static T GetField<T>(string Field, string Table, string Where)
+        {
+            string sql = string.Format("select {0} from {1} where {2}", Field, Table, (Where.Equals(string.Empty) ? "1=1" : Where));
+
+            //get the DataService to get a connection string to the database
+            Sage.Platform.Data.IDataService datasvc = Sage.Platform.Application.ApplicationContext.Current.Services.Get<Sage.Platform.Data.IDataService>();
+            using (System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection(datasvc.GetConnectionString()))
+            {
+                conn.Open();
+                using (OleDbCommand cmd = new OleDbCommand(sql, conn))
+                {
+                    object fieldval = cmd.ExecuteScalar();
+                    return fieldval == DBNull.Value ? default(T) : (T)fieldval;
+                }
+            }
+        }
+        private static DateTime Timelessize(DateTime dt)
+        {
+            DateTime timelessized = new DateTime(dt.Year, dt.Month, dt.Day, 00, 00, 05);
+            return timelessized;
+        }
+        public static string Left(string text, int length)
+        {
+            if (text != null)
+            {
+
+                if (length < 0)
+                    throw new ArgumentOutOfRangeException("length", length, "length must be > 0");
+                else if (length == 0 || text.Length == 0)
+                    return "";
+                else if (text.Length <= length)
+                    return text;
+                else
+                    return text.Substring(0, length);
+            }
+            else
+            {
+                //Null String entered
+                return string.Empty;
+            }
+        }
+        #endregion
     }
 }
