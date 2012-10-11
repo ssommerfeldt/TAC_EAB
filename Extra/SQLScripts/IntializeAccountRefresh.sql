7db4ccab9e71e830801998e62d46685df7c4939f
ssommerfeldt <ssommerfeldt@gmail.com>
2012-10-11 14:23:47 -0600

    SSIS AccountRefresh INSERTs

---
+++ Extra/SQLScripts/IntializeAccountRefresh.sql
@@ -0,0 +1,12 @@
+----------------------------------------
+-- Initialize 
+--=======================================
+Delete from sysdba.ADDRESS where ENTITYID in (Select Accountid from sysdba.ACCOUNT )
+Truncate Table sysdba.Account
+Truncate Table sysdba.AccountSummary
+Truncate Table sysdba.ERPTradingAccount
+Truncate Table sysdba.AccountFinancial
+
+Delete from sysdba.ADDRESS where ENTITYID in (Select CONTACTID from sysdba.CONTACT )
+Truncate Table sysdba.CONTACT
+
