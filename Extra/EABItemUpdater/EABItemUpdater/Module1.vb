61d21d4d860e78a5fab845a9c77bba0741b6e189
ssommerfeldt <ssommerfeldt@gmail.com>
2013-11-04 09:34:11 -0700

    Add SalesOrder Performance Improvements

--- Extra/EABItemUpdater/EABItemUpdater/Module1.vb
+++ Extra/EABItemUpdater/EABItemUpdater/Module1.vb
@@ -7,8 +7,13 @@ Module Module1
         ' Ensure the Commandline args are Enclosed by Double Quotes othersize it parses the Connection string into separate Arguments"
         '=====================================================================================================================================
         Dim s() As String = System.Environment.GetCommandLineArgs()
-        Console.WriteLine(s(1))
-        WriteStatusLog(s(1) & Now.ToString)
+        If (s.Length = 1) Then
+            'Means there is only 1 Argument so Not enough to Run the App.
+            Console.WriteLine("Not Enough Args")
+            Exit Sub
+        End If
+        Console.WriteLine("============ Started ==========================")
+
 
         '======================================================
         ' Get Connection String and Validation Type from Args
@@ -17,6 +22,7 @@ Module Module1
         Dim _Accountid As String = ""
         Dim _RoutinType As String = ""
         ParseCommandLine(s(1), _RoutinType, _strCon, _Accountid)
+        WriteStatusLog("Started RoutineType " & _RoutinType & " " & Now.ToString)
 
         '=========================================================
         ' Get Users AccountManagerId's WareHouse 
@@ -28,15 +34,50 @@ Module Module1
         Dim SQL = ""
         Select Case _RoutinType
             Case "All"
-
+                ProcessAllAccounts(_strCon)
             Case "Account"
                 ProcessAccount(_Accountid, _strCon)
 
         End Select
 
+        WriteStatusLog("Finnished RoutineType " & _RoutinType & " " & Now.ToString)
 
 
 
+
+    End Sub
+    Sub ProcessAllAccounts(ByVal strConn As String)
+        Dim objConn As New OleDbConnection(strConn)
+        Dim Accountid As String
+        
+            Dim SQL As String
+            SQL = "  SELECT DISTINCT sysdba.ACCOUNT.ACCOUNTID"
+            SQL = SQL & " FROM         sysdba.ACCOUNT INNER JOIN"
+            SQL = SQL & "                      sysdba.STOCKCARDITEMS ON sysdba.ACCOUNT.ACCOUNTID = sysdba.STOCKCARDITEMS.ACCOUNTID"
+        Try
+            objConn.Open()
+            Dim objCMD As OleDbCommand = New OleDbCommand(SQL, objConn)
+            Dim objReader As OleDbDataReader = objCMD.ExecuteReader()
+            If objReader.HasRows Then
+                Do While objReader.Read()
+                    Accountid = objReader.GetString(0)
+
+                    ProcessAccount(Accountid, strConn) ' Process all the StockCards For all the Accounts
+
+                Loop
+            Else
+
+
+            End If
+            objReader.Close()
+        Catch ex As Exception
+            Dim EventMessage As String
+            EventMessage = ex.Message & Chr(10) & Chr(13) & "In Function " & "GetAccountInfo"
+            WriteToEventLog(EventMessage, , Diagnostics.EventLogEntryType.Error)
+            'MsgBox(ex.Message)
+        Finally
+            If objConn.State = Data.ConnectionState.Open Then objConn.Close()
+        End Try
     End Sub
 
     Sub ProcessAccount(ByVal Accountid As String, ByVal strConn As String)
