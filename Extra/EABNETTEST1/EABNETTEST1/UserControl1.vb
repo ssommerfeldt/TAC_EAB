00f1cd56a3103bdbbc5fe954c37dba7301e5562d
ssommerfeldt <ssommerfeldt@gmail.com>
2020-10-20 09:51:08 -0600

    Optimization Changes

---
+++ Extra/EABNETTEST1/EABNETTEST1/UserControl1.vb
@@ -0,0 +1,18 @@
+﻿
+Imports System.Data.OleDb
+
+Public Class UserControl1 : Inherits FX.SalesLogix.NetExtensionsHelper.SalesLogixControl
+
+
+    Private Sub UserControl1_SalesLogixRecordChanged(ByVal RecordID As String)
+        'Using conn As OleDbConnection = New OleDbConnection(Me.SlxApplication.ConnectionString)
+        '    conn.Open()
+
+        '    Using da As OleDbDataAdapter = New OleDbDataAdapter(String.Format("select lastname as LastName, firstname as FirstName, type as Type from contact where accountid = '{0}'", RecordID), conn)
+        '        Dim table As DataTable = New DataTable()
+        '        da.Fill(table)
+        '        'dataGridView1.DataSource = table
+        '    End Using
+        'End Using
+    End Sub
+End Class
---
+++ Extra/EAB_NETExtention/EAB_NETExtention/SyncProducts.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SyncProducts
+    Inherits FX.SalesLogix.NetExtensionsHelper.SalesLogixDialog
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.lblFinalMessage = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Info
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(482, 71)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Trebuchet MS", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(59, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(398, 37)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Sync Disconnected Products"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.EAB_NETExtention.My.Resources.Resources.Arrow___right
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 23)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(41, 36)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(24, 90)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(433, 23)
+        Me.ProgressBar1.TabIndex = 1
+        Me.ProgressBar1.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Button1.Location = New System.Drawing.Point(188, 177)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'lblFinalMessage
+        '
+        Me.lblFinalMessage.Font = New System.Drawing.Font("Trebuchet MS", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFinalMessage.Location = New System.Drawing.Point(61, 116)
+        Me.lblFinalMessage.Name = "lblFinalMessage"
+        Me.lblFinalMessage.Size = New System.Drawing.Size(350, 58)
+        Me.lblFinalMessage.TabIndex = 3
+        Me.lblFinalMessage.Text = "Ensure you run your Sync Client to push changes to Head Office"
+        Me.lblFinalMessage.Visible = False
+        '
+        'SyncProducts
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(482, 212)
+        Me.Controls.Add(Me.lblFinalMessage)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "SyncProducts"
+        Me.Text = "Sync Products"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents lblFinalMessage As System.Windows.Forms.Label
+End Class
